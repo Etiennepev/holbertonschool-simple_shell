5c182a5a569e43a224ed60a70eb2c288b0cb113a
Etiennepev <etienneloichot@gmail.com>
2025-01-09 13:54:52 +0100

    Adding the _getenv function

---
+++ _getenv.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * _getenv - Searches for an environment variable by name.
+ * @name: The name of the environment variable.
+ *
+ * Return: The value of the environment variable, or NULL if not found.
+ */
+
+char *_getenv(const char *name)
+{
+	int i, name_len;
+	char *env_var;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		env_var = environ[i];
+		name_len = strlen(name);
+		if (strncmp(env_var, name, name_len) == 0 && env_var[name_len] == '=')
+			return (env_var + name_len + 1);
+	}
+	return (NULL);
+}
