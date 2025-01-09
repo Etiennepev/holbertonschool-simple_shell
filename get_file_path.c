cf9638eab230d6b6dac24738a41bc91473206966
Etiennepev <etienneloichot@gmail.com>
2025-01-09 13:55:51 +0100

    Creating the get file path file

---
+++ get_file_path.c
@@ -0,0 +1,52 @@
+#include "shell.h"
+
+/**
+ * get_file_path - Get's the full path of the file
+ * @file_name: Argument name
+ *
+ * Return: The full path argument to the file
+ */
+
+char *get_file_path(char *file_name)
+{
+	char *path = _getenv("PATH");
+	char *directory, *path_copy, *full_path;
+	size_t len;
+
+	if (!path)
+		return (NULL);
+
+	path_copy = strdup(path);
+	printf("après l'appel de fonction execute command\n");
+	if (!path_copy)
+	{
+		perror("strdup failed");
+		return (NULL);
+	}
+	directory = strtok(path, ":");
+
+	while (directory != NULL)
+	{
+		len = strlen(directory + strlen(file_name) + 2);
+		full_path = malloc(len);
+		if (!full_path)
+		{
+			perror("malloc failed");
+			free(path_copy);
+			return (NULL);
+		}
+		strcpy(full_path, directory);
+		strcat(full_path, "/");
+		strcat(full_path, file_name);
+
+		if (access(full_path, F_OK) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
+		free(full_path);
+		directory = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return (NULL);
+}
