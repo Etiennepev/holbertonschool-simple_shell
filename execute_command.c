1d70f264ef12caae51f4028e77e4322e6f19721b
Etiennepev <etienneloichot@gmail.com>
2025-01-09 13:55:24 +0100

    Modifying all the file execute_command

--- execute_command.c
+++ execute_command.c
@@ -10,25 +10,49 @@ int execute_command(char **args)
 {
 	pid_t child_pid;
 	int status;
+	char *path;
+
+	path = get_file_path(args[0]);
+	if (path == NULL)
+	{
+		perror("path not found");
+		return (1);
+	}
 
 	child_pid = fork();
 	if (child_pid == -1)
 	{
 		perror("Failed to create process");
-		exit(41);
+		exit(0);
 	}
 
 	if (child_pid == 0)
 	{
-		if (execve(args[0], args, NULL) == -1)
+		if (execve(path, args, environ) == -1)
 		{
 			perror("Failed to execute");
-			exit(97);
+			exit(0);
 		}
 	}
 	else
 	{
 		wait(&status);
 	}
+	free(path);
 	return (0);
+}
+
+/*
+* free_array - 
+*
+*
+*/
+
+void free_array(char **tokens)
+{
+	int i;
+
+	for (i = 0; tokens[i] != NULL; i++)
+		free(tokens[i]);
+	free(tokens);
 }
